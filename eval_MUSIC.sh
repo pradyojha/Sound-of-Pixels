5da6a7f9595c03a55530340bcc83df1d4974f306
Pradyumna Ojha <pojha@usf.edu>
2020-08-27 10:31:26 -0400

    updated path for data

--- eval_MUSIC.sh
+++ eval_MUSIC.sh
@@ -3,8 +3,8 @@
 OPTS=""
 OPTS+="--mode eval "
 OPTS+="--id MUSIC-2mix-LogFreq-resnet18dilated-unet7-linear-frames3stride24-maxpool-binary-weightedLoss-channels32-epoch100-step40_80 "
-OPTS+="--list_val /data/pojha/research/source_code/sound-of-pixels/data/val.csv "
-OPTS+="--list_train /data/pojha/research/source_code/sound-of-pixels/data/train.csv "
+OPTS+="--list_val /data/pojha/research-data/Sound-of-Pixels/val.csv "
+OPTS+="--list_train /data/pojha/research-data/Sound-of-Pixels/train.csv "
 
 # Models
 OPTS+="--arch_sound unet7 "
@@ -30,9 +30,10 @@ OPTS+="--audLen 65535 "
 OPTS+="--audRate 11025 "
 
 #python3 -m pdb main.py $OPTS
-source activate py36
+#source /export/apps/anaconda3-gpu/etc/profile.d/conda.sh
+conda activate base
 # if you face libstd++ or libgcc issue , install addtional conda packages in the prerequistes.txt for libstd and libgcc 
 # and enable the below line as appropiate with the path
-#export LD_LIBRARY_PATH=/home/pojha/.conda/envs/py36/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/pojha/.conda/envs/py36/lib/:$LD_LIBRARY_PATH
 python main.py $OPTS
-source deactivate
+conda deactivate
